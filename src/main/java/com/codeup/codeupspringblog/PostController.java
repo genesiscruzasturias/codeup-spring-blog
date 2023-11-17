8e8f22ed9c7646724ad2f1d7e66d6af598ff9b90
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-11-17 16:54:15 -0600

    commit

--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -1,7 +1,10 @@
 package com.codeup.codeupspringblog;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
@@ -15,6 +18,10 @@ public class PostController {
     @RequestMapping(path ="/posts/{id}", method = RequestMethod.GET)
     @ResponseBody
     public String individualPost(@PathVariable int id){
+        ArrayList <String> individualPost = new ArrayList<>();
+        individualPost.add("Here's one");
+
+
         return "view an individual post";
     }
 
@@ -30,6 +37,8 @@ public class PostController {
     @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
     @ResponseBody
     public String createNewPost(){
+
+
         return "create a new post";
     }
 
